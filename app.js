5f1048011dc268b9eeef2ec1115bd7c074ba2c95
Abhishek borana <76511435+abhishekborana@users.noreply.github.com>
2023-05-16 11:59:53 +0000

    setting up strcture for user schema

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 require('dotenv').config() // Adding Config file here
+require("./database/database").connect()
 const express = require('express');
 
 const app = express(); 
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hola ! Amigos</h1>");
 })
+
+
+app.post("/register", async (req,res)=>{
+    try{
+        // get all data from body
+
+        // all the data should exists
+
+        // check if user already exists
+
+        // encrpty the password
+
+        // save user in db 
+
+        // generate token for user and send it
+
+    }
+    catch(error){
+        console.log(error);
+    }
+})
+
 
 module.exports = app;
--- database/database.js
+++ database/database.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URL = process.env;
+const {MONGODB_URL} = process.env;
 
 exports.connect = ()=>{
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
-    .then()
+    .then(
+        console.log("Database Connected")
+    )
     .catch((error)=>{
         console.log(`Error While Connecting to Database`);
         console.log(error);
         process.exit(1);
     })
 }
+
